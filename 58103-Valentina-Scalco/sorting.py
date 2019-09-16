c072f08f5fc697c6f641d0d73c7f2dedd5ea4ec1
valenscalco <mv.scalco@alumno.um.edu.ar>
2019-09-16 18:04:17 -0300

    commit

--- 58103-Valentina-Scalco/sorting.py
+++ 58103-Valentina-Scalco/sorting.py
@@ -2,8 +2,9 @@ class sort:
     def bubbleSort(self, lista):
         n = len(lista)
         i = 0
-        for i in range (1, n):
-            for j in range (1-n):
-                if lista [j] > lista[j+1]:
-                   lista[j], lista[j+1] = lista[j+1], lista[j] 
+        j = 0
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if lista[j] > lista[j+1]:
+                    lista[j], lista[j+1] = lista[j+1], lista[j]
         return lista
